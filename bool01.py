c0583d822cb2c72699c9a41906e6e9dfa28412e8
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-14 11:52:38 +0500

    1 misol

--- bool01.py
+++ bool01.py
@@ -1,11 +1,13 @@
 def main(a,b):
-    """
-    Check the logic "they are equal"
-    Args:
+     """
+     Check the logic "they are equal"
+     Args:
         a: int
         b: int
-    Returns:
+     Returns:
         bool
-    """
-    # Write your code here
-    return 
+     """
+     # Write your code here
+
+     return (a==b) 
+print(main(1,1))
